bf07e70f02e695be676bbd57609a747c29f559f2
André Wittwer <andre.wittwer@gmail.com>
2024-02-14 21:50:25 +0100

    feature: add brew import

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install all from Brewfile
+
+brew bundle
+
 # Enable special handling to prevent expansion to a
 # literal '/tmp/backup/*' when no matches are found. 
 shopt -s nullglob
@@ -12,4 +16,5 @@ done
 # Unset shell option after use, if desired. Nullglob
 # is unset by default.
 shopt -u nullglob
+
 
