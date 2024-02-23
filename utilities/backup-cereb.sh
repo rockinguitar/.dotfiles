95facf19aefe4c814e1e82b5f2b154c39dc23e22
André Wittwer <andre.wittwer@gmail.com>
2024-02-23 11:06:55 +0100

    feat: backup script for cerebrum

---
+++ utilities/backup-cereb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+  
+# Script for backing up the cerebrum-vault to github
+  
+CEREBRUMPATH=~/Documents/Cerebrum-Vault
+GITHUBCEREBPATH=~/Repos/cerebrum-vault/
+  
+cd $GITHUBCEREBPATH
+shopt -s extglob
+rm -r !(.gitignore|.git)
+cd $CEREBRUMPATH
+cp -r . $GITHUBCEREBPATH
+cd -
+  
+if [[ `git status --porcelain` ]]; then
+  # Changes
+  git add .
+  git commit -m "Backup on $(date)"
+  git push
+fi
