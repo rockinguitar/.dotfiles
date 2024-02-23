999ae52e3002159977909bf6702501ea8ff4b3d8
André Wittwer <andre.wittwer@gmail.com>
2024-02-23 10:47:32 +0100

    feat: update brew automatically

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 
 brew bundle
 
+# Configure brew to update itself all 12 hours
+
+brew  autoupdate start 43200 --upgrade --cleanup --immediate --sudo
+
 # Stow with verbose add -n to just simulate
 
 stow alacritty git zsh -v
